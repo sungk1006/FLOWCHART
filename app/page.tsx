a9846a65e94b7e657405828d3b7558da85b0b2e1
sungk1006 <sungk100666@gmail.com>
2026-03-23 00:32:52 +0900

    disable ssr for homepage client app

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,9 @@
-import FlowchartClient from "./FlowchartClient";
+import dynamic from "next/dynamic";
+
+const FlowchartClient = dynamic(() => import("./FlowchartClient"), {
+  ssr: false,
+  loading: () => <div style={{ padding: 24 }}>Loading...</div>,
+});
 
 export default function Page() {
   return <FlowchartClient />;
